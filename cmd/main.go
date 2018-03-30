32e3f414815077fdf32db3c6e44372069c1cf90a
Michael Telford <michael.telford@live.com>
2018-03-31 00:43:30 +0100

    Added echo middleware

--- cmd/main.go
+++ cmd/main.go
@@ -4,13 +4,20 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
 func main() {
 	e := echo.New()
 
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
+	e.Use(middleware.Logger()) // Response logging only
+	e.Use(middleware.Secure())
+	e.Use(middleware.Recover())
+
+	e.GET("/greet", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, map[string]string{
+			"data": "Hello, World!",
+		})
 	})
 
 	e.Logger.Fatal(e.Start(":8000"))
